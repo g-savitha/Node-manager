a8c8d7aae8de3c9c9ed2a9b71cf915869fd2522d
Savitha <savitha.gollamudi@gmail.com>
2020-07-15 17:02:37 +0530

    Give access to files and ensure they exist

--- index.js
+++ index.js
@@ -4,22 +4,28 @@ const log = console.log;
 const chokidar = require("chokidar");
 const debounce = require("lodash.debounce");
 const program = require("caporal");
+const fs = require("fs");
 
 program
   .version("1.0.0")
   .argument("[filename]", "Name of the file to be executed")
-  .action((args) => {
-    log(args);
+  .action(async ({ filename }) => {
+    const name = filename || "index.js";
+    try {
+      await fs.promises.access(name);
+    } catch (err) {
+      throw new Error(`Couldn't find the file ${name}`);
+    }
+    const start = debounce(() => {
+      log("Starting the program");
+    }, 100);
+
+    // One-liner to watch over current directory
+    chokidar
+      .watch(".")
+      .on("add", start)
+      .on("change", start)
+      .on("unlink", start);
   });
 
 program.parse(process.argv);
-// const start = debounce(() => {
-//   log("Starting the program");
-// }, 100);
-
-// // One-liner for current directory
-// chokidar
-//   .watch(".")
-//   .on("add", start)
-//   .on("change", () => log("file changed"))
-//   .on("unlink", () => log("file unlinked"));
